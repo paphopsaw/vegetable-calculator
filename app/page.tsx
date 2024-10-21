b21ff317b43ad6197dd3fc43c61050f8527bce14
Paphop Sawasdee <paphop.staw@gmail.com>
2024-10-21 23:19:45 +0900

    Add comment and linting

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,21 @@
-'use client'
+"use client";
 
-import CardCollection, { CardData } from '@/app/ui/card-collection'
-import Summary from '@/app/ui/summary'
-import { useState, useEffect } from 'react'
+import CardCollection, { CardData } from "@/app/ui/card-collection";
+import Summary from "@/app/ui/summary";
+import { useState, useEffect } from "react";
 
 export default function Home() {
-  const [state, setState]  = useState<CardData[]>([]);
+  const [state, setState] = useState<CardData[]>([]);
 
   useEffect(() => {
-      fetchCSVData();
+    fetchCSVData();
   }, []);
 
   async function fetchCSVData() {
-    const csvUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ_SPSfqcF3AcCJmmmzVLxbbBvInozEe_RR3l4qd583d4jTDPgWteK4qQOPW2ddGfC-2BjGZsx2rjKf/pub?output=csv';
-    const response = await fetch(csvUrl)
+    // Change url to the one of your choice.
+    const csvUrl =
+      "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ_SPSfqcF3AcCJmmmzVLxbbBvInozEe_RR3l4qd583d4jTDPgWteK4qQOPW2ddGfC-2BjGZsx2rjKf/pub?output=csv";
+    const response = await fetch(csvUrl);
     const csvText = await response.text();
     setState(parseCSV(csvText));
   }
@@ -22,14 +24,14 @@ export default function Home() {
     const rows = text.split(/\r?\n/);
     const data = [];
     for (let i = 0; i < rows.length; i++) {
-        const rowData = rows[i].split(',');
-        const rowObject:CardData = {
-          id: i,
-          name: rowData[0],
-          price: parseInt(rowData[1]),
-          count: 0
-        };
-        data.push(rowObject);
+      const rowData = rows[i].split(",");
+      const rowObject: CardData = {
+        id: i,
+        name: rowData[0],
+        price: parseInt(rowData[1]),
+        count: 0,
+      };
+      data.push(rowObject);
     }
     return data;
   }
@@ -37,7 +39,7 @@ export default function Home() {
   function handleUpdate(increment: number, id: number) {
     const nextState = state.map((c, i) => {
       if (i === id) {
-        const newCard:CardData = {
+        const newCard: CardData = {
           ...c,
           count: Math.max(c.count + increment, 0),
         };
@@ -50,26 +52,33 @@ export default function Home() {
   }
 
   function getTotal() {
-    return state.map(item => item.count * item.price).reduce((partialSum, a) => partialSum + a, 0);
+    return state
+      .map((item) => item.count * item.price)
+      .reduce((partialSum, a) => partialSum + a, 0);
   }
 
   function clear() {
     const nextState = state.map((c, i) => {
-      const newCard:CardData = {
+      const newCard: CardData = {
         ...c,
         count: 0,
-      }
-      return newCard
+      };
+      return newCard;
     });
     setState(nextState);
   }
 
   return (
-    <main className='flex flex-col'>
+    <main className="flex flex-col">
       <h1>計算機</h1>
-      <CardCollection cardList={state} onUpdate={handleUpdate}/>
-      <Summary total={getTotal()}/>
-      <button className='text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900' onClick={clear}>クリア</button>
+      <CardCollection cardList={state} onUpdate={handleUpdate} />
+      <Summary total={getTotal()} />
+      <button
+        className="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+        onClick={clear}
+      >
+        クリア
+      </button>
     </main>
-  )
+  );
 }
--- app/ui/summary.tsx
+++ app/ui/summary.tsx
@@ -1,9 +1,9 @@
-'use client'
+"use client";
 
-export default function Summary({total}: {total: number}) {
+export default function Summary({ total }: { total: number }) {
   return (
     <div className="flex">
       <p className="text-xl font-bold my-10">合計: {total}</p>
     </div>
-  )
+  );
 }
